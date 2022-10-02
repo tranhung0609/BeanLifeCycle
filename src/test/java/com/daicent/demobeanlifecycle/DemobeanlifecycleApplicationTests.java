230ca125a0ba2cd584ebd83d34add32055dc6555
tranhung0609 <trvanhung.nhk.0609@gmail.com>
2022-10-02 12:00:25 +0700

    bean life cycle

---
+++ src/test/java/com/daicent/demobeanlifecycle/DemobeanlifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daicent.demobeanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobeanlifecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
